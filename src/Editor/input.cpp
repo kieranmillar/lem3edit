0b4d23e21999e6cce68ec34624fe4e12147ab10c
Kieran Millar <kieranmccleanmillar@gmail.com>
2017-11-26 18:29:09 +0000

    Minor tweak to frame-skipping rules

--- src/Editor/input.cpp
+++ src/Editor/input.cpp
@@ -524,7 +524,7 @@ void Editor_input::handleEvents(SDL_Event event)
 
 			//Only draw if time between this frame and last was neither too long nor too short
 			Uint32 ticksSinceLastFrame = SDL_GetTicks() - editor_ptr->gameFrameTick;
-			if (ticksSinceLastFrame <= 35 && ticksSinceLastFrame >= 3)
+			if (ticksSinceLastFrame <= 35 && ticksSinceLastFrame >= 30)
 			{
 				editor_ptr->gameFrameTick = SDL_GetTicks();
 				canvas_ptr->draw();
