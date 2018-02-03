c06355812dc016631639d5b68f5f72ef4c88c288
Kieran Millar <kieranmccleanmillar@gmail.com>
2018-02-03 15:50:40 +0000

    Increase version number to 1.2

--- src/lem3edit.cpp
+++ src/lem3edit.cpp
@@ -43,8 +43,8 @@ using namespace std;
 namespace fs = std::experimental::filesystem::v1;
 
 const char *prog_name = "lem3edit";
-const char *prog_ver = "1.1";
-const char *prog_date = "12/01/2018";
+const char *prog_ver = "1.2";
+const char *prog_date = "03/02/2018";
 
 void version(void);
 
